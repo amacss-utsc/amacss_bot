13c5ec821d93e6c40ecc92edde039748fa683422
murphylee10 <murphylee2004@gmail.com>
2023-04-01 10:00:00 +0200

    exec feature update

--- cog_loader.py
+++ cog_loader.py
@@ -1,5 +1,7 @@
 from common.murphy_lee.hello.hello import MurphyHello
+from exec.murphy_lee.channel_admin.channel_admin import MurphyChannelAdmin
 
 # MUST ADD YOUR COGS HERE FOR YOUR COMMANDS TO WORK
 def cog_loader(bot):
   bot.add_cog(MurphyHello(bot))
+  bot.add_cog(MurphyChannelAdmin(bot))
