4560a2154d9a65cdd67ea974a31ededfe1244556
murphylee10 <murphylee2004@gmail.com>
2024-10-09 16:53:18 -0400

    fix: cleanup and migrate to discord.py

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.10
-WORKDIR /bot
-COPY deps.txt /bot/
-RUN pip install -r deps.txt
-COPY . /bot
-CMD python main.py
---
+++ cogs/ping.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Ping cog is ready')
+
+    @commands.command(name='ping')
+    async def ping(self, ctx):
+        await ctx.send('Pong!')
+
+async def setup(bot):
+    await bot.add_cog(Ping(bot))
