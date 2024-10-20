8a54f2bbc3a7dbc0cf682cb0eb7b10cc90dc93fc
m2zhang <marilyn2zhang@gmail.com>
2024-10-20 16:32:49 -0400

    fix link to affirmations.txt file

--- cogs/generic/ping.py
+++ cogs/generic/ping.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import random
+import os
 
 class Ping(commands.Cog):
     def __init__(self, bot):
@@ -19,14 +20,15 @@ class Ping(commands.Cog):
     async def affirm(self, interaction: discord.Interaction):
         # Load affirmations from the text file
         try:
-            with open('affirmations.txt', 'r') as file:
+            file_path = os.path.join(os.path.dirname(__file__), 'affirmations.txt')
+            with open(file_path, 'r') as file:
                 affirmations = file.readlines()
             # Strip newline characters + choose random affirmation
             random_affirmation = random.choice([affirmation.strip() for affirmation in affirmations])
             await interaction.response.send_message(random_affirmation)
         except FileNotFoundError:
             # Affirmation file not found :OO
-            await interaction.response.send_message("Try again later ¯\_(ツ)_/¯")
+            await interaction.response.send_message("Try again later ¯\\_(ツ)_/¯")
 
 
 async def setup(bot):
